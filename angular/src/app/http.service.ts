f9cdae62c4263c827d0087a884f2c46542cf4beb
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-13 15:50:27 -0700

    debugging

--- angular/src/app/http.service.ts
+++ angular/src/app/http.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class HttpService {
-
-  constructor(private _http: HttpClient)  {
+  constructor(private _http: HttpClient) {
     this.getTasks();
   }
-}
 
-getTasks(){
-  let tempObservable = this._http.get('/tasks');
-  tempObservable.subscribe(data => console.log('This are our tasks!', data));
+  getTasks(){
+    let tempObservable = this._http.get('/tasks');
+    tempObservable.subscribe(data => console.log('This are our tasks!', data));
+  }
 }
